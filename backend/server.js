854c5c2d962d8737c9116427a7f3909f1ffd6938
Srikanth5253 <srikanth.boda@motivitylabs.com>
2026-06-22 18:15:07 +0530

    Docker setup

--- backend/server.js
+++ backend/server.js
@@ -27,19 +27,6 @@ const server = http.createServer(app);
 
 app.use(express.json());
 
-// app.set("trust proxy", 1);
-// app.use(
-//   cors({
-//     origin: process.env.CLIENT_URL,
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: [
-//       "Content-Type",
-//       "Authorization",
-//     ],
-//   })
-// );
-
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
